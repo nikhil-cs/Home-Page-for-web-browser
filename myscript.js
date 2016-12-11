887b4815431a0230c108c62c911b03a7af1e9492
Nikhil Chaudhary <nike3351@gmail.com>
2016-12-11 12:27:24 +0530

    First commit

---
+++ myscript.js
@@ -0,0 +1,20 @@
+function searchYoutube(){
+	var str = document.getElementById("youtubeTxt").value;
+	var res = str.split(" ");
+	 //document.getElementById("demo").innerHTML = res;
+	 var link = "https://www.youtube.com/rres?search_query=";
+	 for (var i = 0; i < res.length; i++) {
+	 	link = link + var[i] + "+";
+	 	window.open(link);
+	 }
+}
+function searchIdope(){
+	var str = document.getElementById("idopeTxt").value;
+	var res = str.split(" ");
+	 //document.getElementById("demo").innerHTML = res;
+	 var link = "https://idope.se/torrent-list/";
+	 for (var i = 0; i < res.length; i++) {
+	 	link = link + var[i] + "%20";
+	 	window.open(link);
+	 }
+}
